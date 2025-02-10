c77302e571bd8dc8c352c8c81094a015bcc7bb25
FreshJourney <freshjourney00@gmail.com>
2025-02-10 23:19:54 +0530

    rate limmitter

--- server/middleware/rateLimiter2.js
+++ server/middleware/rateLimiter2.js
@@ -1,7 +1,10 @@
 const count=0;
 function rateLimiter2(){
       try {
-            if()
+            if(if count>0)
+            {
+                  res.return();
+            }
       } catch (error) {
             
       }
